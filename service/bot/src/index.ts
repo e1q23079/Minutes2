def17e04c36b50a7c7c0405f22c7ff10e08084d5
Q23079 <156163994+e1q23079@users.noreply.github.com>
2026-08-28 02:14:01 +0000

    feat(bot): 環境変数の管理をdotenvで追加し、APIキーとボイスチャンネルIDのチェックを実装

--- service/bot/src/index.ts
+++ service/bot/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello, World!");
+import "dotenv/config";
+
+const API_KEY = process.env.API_KEY;
+if (!API_KEY) {
+  throw new Error("APIキーが環境変数に定義されていません。");
+}
+
+const VOICE_CHANNEL_ID = process.env.VOICE_CHANNEL_ID;
+if (!VOICE_CHANNEL_ID) {
+  throw new Error("VOICE_CHANNEL_IDが環境変数に定義されていません。");
+}
